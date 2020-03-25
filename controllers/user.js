1fa4624f36436893834c94479c6415c6cd30e08b
Coder-Hex <rohansharma.8574@gmail.com>
2020-03-25 11:27:31 +0530

    added Custom Titles

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,17 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const userProfile = (req, res) => res.render("userProfile");
-export const editProfile = (req, res) => res.render("editProfile");
-export const editPassword = (req, res) => res.render("editPassword");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
+
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+
+export const userProfile = (req, res) =>
+  res.render("userProfile", { pageTitle: "User Profile" });
+
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const editPassword = (req, res) =>
+  res.render("editPassword", { pageTitle: "Edit Password" });
