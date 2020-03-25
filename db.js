be1c3518780e995443a234493af0c9350064f0fc
Coder-Hex <rohansharma.8574@gmail.com>
2020-03-25 15:13:40 +0530

    Added Videos

---
+++ db.js
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    id: 2222,
+    title: "Video 1",
+    description: "This is First Video",
+    views: 24,
+    videoURL: "http://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 23232,
+      name: "Krishna Sharma",
+      email: "krishna@gmail.com"
+    }
+  },
+  {
+    id: 2232322,
+    title: "Video 2",
+    description: "This is Second Video",
+    views: 24,
+    videoURL: "http://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 23232,
+      name: "Suyash Sharma",
+      email: "krishna@gmail.com"
+    }
+  }
+];
