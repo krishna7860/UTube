d6fa644f9b5ed20ed05c020b3363cc50bf0b88c6
Coder-Hex <rohansharma.8574@gmail.com>
2020-03-25 13:00:29 +0530

    Added Forms

--- router/user.js
+++ router/user.js
@@ -10,8 +10,8 @@ import {
 const userRouter = express.Router();
 
 userRouter.get(routes.home, users);
-userRouter.get(routes.userProfile, userProfile);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.editPassword, editPassword);
+userRouter.get(routes.userProfile, userProfile);
 
 export default userRouter;
